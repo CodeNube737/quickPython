5854ded848e9a018049fa692d68a1cd5288023f7
CodeNube737 <158295465+CodeNube737@users.noreply.github.com>
2025-08-08 17:29:39 -0700

    feat: built-in logic functions for sheet formulas

---
+++ pySheets/functions.py
@@ -0,0 +1,13 @@
+def IF(condition, true_val, false_val):
+    return true_val if condition else false_val
+
+def AND(*args):
+    return all(args)
+
+def OR(*args):
+    return any(args)
+
+def NOT(arg):
+    return not arg
+
+# You can add more functions here as needed.
